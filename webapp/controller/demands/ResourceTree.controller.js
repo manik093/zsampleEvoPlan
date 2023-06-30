9ecd78dfc75fb655a4b2ea60a02c4fe54b472054
Manik Saluja <manik093@gmail.com>
2023-06-30 11:06:23 +0000

    changes in view 1

---
+++ webapp/controller/demands/ResourceTree.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"com/sample/zcheckplan/controller/common/AssignmentsController"
+], function (Controller) {
+	"use strict";
+	return Controller.extend("com.sample.zcheckplan.controller.demands.ResourceTree", {
+		onInit: function () {
+			
+		}
+	});
+});
